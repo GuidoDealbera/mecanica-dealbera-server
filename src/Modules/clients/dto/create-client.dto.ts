9365e96ac937a7f265e1e753553dc1987c12cec3
Guido <guidojdealbera@gmail.com>
2025-04-23 19:07:11 -0300

    fix client dto

--- src/Modules/clients/dto/create-client.dto.ts
+++ src/Modules/clients/dto/create-client.dto.ts
@@ -1,30 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 
 export class CreateClientDto {
   @ApiProperty()
   @IsString()
-  @IsNotEmpty({message: 'El nombre completo es requerido'})
+  @IsNotEmpty({ message: 'El nombre completo es requerido' })
   fullname: string;
 
   @ApiProperty()
   @IsString()
-  @IsPhoneNumber('AR', {message: 'Inserte un número de teléfono válido'})
-  @IsNotEmpty({message: 'El número de teléfono es requerido'})
+  @IsPhoneNumber('AR', { message: 'Inserte un número de teléfono válido' })
+  @IsNotEmpty({ message: 'El número de teléfono es requerido' })
   phone: string;
 
   @ApiProperty()
   @IsString()
-  @IsNotEmpty({message: 'La dirección es requerida'})
+  @IsNotEmpty({ message: 'La dirección es requerida' })
   address: string;
 
   @ApiProperty()
   @IsString()
-  @IsNotEmpty({message: 'La localidad es requerido'})
+  @IsNotEmpty({ message: 'La localidad es requerido' })
   city: string;
 
   @ApiProperty()
   @IsOptional()
-  @IsEmail()
+  @IsEmail({}, { message: 'Email inválido' })
   email?: string;
 }
